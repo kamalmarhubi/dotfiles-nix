00ca078838a30eb7a3bfc3d1adc75c1f0fee487f
Kamal Marhubi <kamal@marhubi.com>
2022-10-17 20:23:59 -0400

    Add minimal flake.{nix,lock}

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Kamal pretends to use nix?";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    homeConfigurations = {
+      "kamal@kx7" = let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [{
+          programs = {
+            home-manager.enable = true;
+          };
+
+          home = {
+            username = "kamal";
+            homeDirectory = "/home/kamal";
+            stateVersion = "22.11";
+          };
+        }];
+      };
+    };
+  };
+}
