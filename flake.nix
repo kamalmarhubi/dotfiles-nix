6574021681dd73dc499c0ef5f39f6c0d701211e9
Kamal Al Marhubi <kamal@marhubi.com>
2022-10-25 21:18:08 -0400

    Ensure nix-daemon.fish is sourced

--- flake.nix
+++ flake.nix
@@ -42,6 +42,16 @@
              EDITOR = "nvim";
            };
          };
+         # For whatever reason, the installer didn't put this somewhere that
+         # fish would see. Since the nix-daemon.fish file guards against
+         # double-sourcing, there's no harm including this in all systems.
+         xdg.configFile."fish/conf.d/nix.fish".text = ''
+           # Nix
+           if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
+             . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
+           end
+           # End Nix
+         '';
        }
      ];
     };
