d9935274bed2befc5d738d59378ab57ce8e7710e
Kamal Al Marhubi <kamal@marhubi.com>
2022-10-31 23:00:20 -0400

    Update todos

--- flake.nix
+++ flake.nix
@@ -3,7 +3,12 @@
 #   - add in plugins I know I like
 #   - figure out keymapping plugin
 #   - really: color scheme?
+#     - acme is ok; need to fix cursor highlight though
+#     - also link various builtins' hl groups to the non-built-in version
 #   - print out new thing to put in packpath after activation? the way to do that would be via neovim not nix
+#     - OR: add way to switch to current generation's packdir and reload everything
+#       - `nvim --headless '+lua print(vim.opt.packpath:get()[1])' +q` will print packdir from new neovim
+#       - v:argv contains arguments of this running nvim: can be used to reset packpath
 {
   description = "Kamal pretends to use nix?";
 
