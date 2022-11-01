fbc73ddcd0f776b137900e5d75a4747358e677ed
Kamal Al Marhubi <kamal@marhubi.com>
2022-10-31 23:00:38 -0400

    neovim: Set termguicolors

--- files/nvim/init.lua
+++ files/nvim/init.lua
@@ -5,4 +5,5 @@ import('Comment', function(m) m.setup() end)
 import('guess-indent', function(m) m.setup() end)
 import('nvim-surround', function(m) m.setup() end)
 import('lazy-lsp', function(m) m.setup() end)
+vim.opt.termguicolors = true
 vim.cmd.colorscheme('acme')
