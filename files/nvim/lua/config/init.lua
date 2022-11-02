c49543641b682c843376568b387b2a38851c2dc8
Kamal Al Marhubi <kamal@marhubi.com>
2022-11-02 17:25:15 -0400

    neovim: Add cabbr for %% as current file's directory

--- files/nvim/lua/config/init.lua
+++ files/nvim/lua/config/init.lua
@@ -1,3 +1,8 @@
+-- Super handy command mode abbreviation for the directory of the current file.
+-- From
+--   http://vim.wikia.com/wiki/Easy_edit_of_files_in_the_same_directory#Using_a_command_line_abbreviation
+vim.cmd[[cabbr <expr> %% expand('%:p:h')]]
+
 import('leap', function(m) m.add_default_mappings() end)
 import('Comment', function(m) m.setup() end)
 import('guess-indent', function(m) m.setup() end)
