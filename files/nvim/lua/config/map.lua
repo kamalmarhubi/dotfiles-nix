28785bc271c737d15bbabf56ed4260c946153611
Kamal Al Marhubi <kamal@wave.com>
2022-11-04 17:33:17 -0400

    neovim: Only add lsp mappings if there's an lsp

--- files/nvim/lua/config/map.lua
+++ files/nvim/lua/config/map.lua
@@ -10,8 +10,3 @@ vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "fin
 vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "grep" })
 vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "recent files" })
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "buffers" })
-
--- Ideally these should be set conditionally... maybe in lsp on_attach?
-wk.register({["<leader>l"] = { name = "lsp" } })
-vim.keymap.set("n", "<leader>lr", "<cmd>Telescope lsp_references<cr>", { desc = "references" })
-vim.keymap.set("n", "<leader>ld", "<cmd>lua vim.lsp.buf.definition()<cr>", { desc = "definition" })
