b0eb9359d67e5e28927ad2e9d351f5ea9ac38b93
Kamal Al Marhubi <kamal@marhubi.com>
2022-12-07 14:53:51 -0500

    home-manager: Temporarily use my patched version

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:kamalmarhubi/home-manager/flake-short-hostname";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
