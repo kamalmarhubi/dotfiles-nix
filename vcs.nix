22cd7675dbd41bba99004f0405dd1afccd50ec3c
Kamal Al Marhubi <kamal@marhubi.com>
2023-06-17 12:06:30 -0400

    git: Set up system-based credential helper

--- vcs.nix
+++ vcs.nix
@@ -20,4 +20,13 @@
 
   xdg.configFile."git/config".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.dataHome}/dotfiles-nix/files/git/config";
   xdg.configFile."git/config.mine".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.dataHome}/dotfiles-nix/files/git/config.mine";
+  xdg.configFile."git/config.system".text = let
+    credentialHelper =
+      if pkgs.stdenv.isLinux then "libsecret"
+      else if pkgs.stdenv.isDarwin then "osxkeychain"
+      else "";
+  in ''
+  [credential]
+  	helper = ${credentialHelper}
+  '';
 }
