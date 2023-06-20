dee803fae11f6c02c3b69d9a65f703877187dafe
Kamal Al Marhubi <kamal@marhubi.com>
2023-06-20 18:49:43 +0000

    wezterm: Fix select word boundary

--- files/wezterm/wezterm.lua
+++ files/wezterm/wezterm.lua
@@ -5,4 +5,7 @@ return {
   font = wezterm.font 'Iosevka Fixed Slab',
   font_size = 14,
   color_scheme = "tokyonight-day",
+  -- Default plus  ▏which is used in nvim borders & indent guides
+  -- Default: https://wezfurlong.org/wezterm/config/lua/config/selection_word_boundary.html
+  selection_word_boundary = " │\t\n{}[]()\"'`",
 }
