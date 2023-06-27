ae63c90a7a2073d1a4beb1d6590fac0464defbff
Kamal Al Marhubi <kamal@marhubi.com>
2023-06-27 02:27:48 +0000

    Update inputs except for neovim

--- files/wezterm/wezterm.lua
+++ files/wezterm/wezterm.lua
@@ -5,7 +5,9 @@ return {
   font = wezterm.font 'Iosevka Fixed Slab',
   font_size = 13,
   color_scheme = "tokyonight-day",
-  -- Default plus  ▏which is used in nvim borders & indent guides
+  -- Default plus
+  --   ▏    used in nvim borders & indent guides
+  --   ─    used in tree output
   -- Default: https://wezfurlong.org/wezterm/config/lua/config/selection_word_boundary.html
-  selection_word_boundary = " │\t\n{}[]()\"'`",
+  selection_word_boundary = " │─\t\n{}[]()\"'`",
 }
