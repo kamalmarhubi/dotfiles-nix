0210a57a92c7fc2cb86db7a5b2b6c40471132b33
Kamal Al Marhubi <kamal@marhubi.com>
2023-07-08 15:35:02 -0400

    fonts: Remove nerdfonts since wezterm handles them

--- fonts.nix
+++ fonts.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
-    (nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})
     (iosevka-bin.override {variant = "sgr-iosevka-fixed";})
     (iosevka-bin.override {variant = "sgr-iosevka-fixed-slab";})
   ];
