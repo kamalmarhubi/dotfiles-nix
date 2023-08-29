add7104dd27c2138c3b21b98ee130f28bf86758d
Kamal Al Marhubi <kamal@marhubi.com>
2023-08-29 19:07:36 -0400

    wave: Set up terraform plugin cache

--- wave.nix
+++ wave.nix
@@ -3,7 +3,9 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  terraformPluginCache = "${config.xdg.cacheHome}/terraform/plugin-cache";
+in {
   programs.fish.shellInit = ''
     source $HOME/.nix-profile/share/asdf-vm/asdf.fish
   '';
@@ -28,4 +30,12 @@
   ];
 
   xdg.configFile."git/config.local".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/home-manager/files/git/config.wave";
+  home.file.".terraformrc".text = ''
+    plugin_cache_dir = "${terraformPluginCache}"
+  '';
+  home.activation.ensureTerraformPluginCache = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    $VERBOSE_ECHO "Ensuring terraform plugin cache directory exists"
+
+    $DRY_RUN_CMD mkdir -p "${terraformPluginCache}"
+  '';
 }
