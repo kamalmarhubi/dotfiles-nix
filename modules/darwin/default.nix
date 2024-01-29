213fa8569898adf6938241edfe4a3d2e464212da
Kamal Al Marhubi <kamal@marhubi.com>
2024-01-29 14:59:53 -0500

    darwin/sudo: pam_tid.so & timeout

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -10,6 +10,10 @@
   programs.fish.enable = true;
   users.users.kamal.home = "/Users/kamal";
   users.users.kamal.name = "kamal";
+  security.pam.enableSudoTouchIdAuth = true;
+  environment.etc."sudoers.d/10-timestamp_timeout".text = ''
+    Defaults        timestamp_timeout=0
+  '';
   home-manager.useGlobalPkgs = true;
   # home-manager.useUserPackages = true;
   home-manager.users.kamal = import ../home-manager;
