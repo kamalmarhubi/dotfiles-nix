568885348a9f40fcca9eee24187bcb909cca4964
Kamal Al Marhubi <kamal@marhubi.com>
2024-04-21 23:16:17 -0400

    add flake registry for nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-23.11-darwin";
+    unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
