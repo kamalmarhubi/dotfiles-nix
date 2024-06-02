af83de487f0a724def57a87c5924ea7dad8fa215
Kamal Al Marhubi <kamal@marhubi.com>
2024-06-02 11:09:15 -0400

    wezterm: rely on SHELL

--- files/wezterm/wezterm.lua
+++ files/wezterm/wezterm.lua
@@ -1,7 +1,6 @@
 local wezterm = require 'wezterm'
 
 return {
-  default_prog = { 'fish', '--login', '--interactive'},
   font = wezterm.font 'Iosevka Fixed Slab',
   font_size = 13,
   use_cap_height_to_scale_fallback_fonts = true,
