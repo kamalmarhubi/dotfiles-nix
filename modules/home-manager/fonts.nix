385469c772e64f95f7b9353f6aff4702f8a035c6
Kamal Al Marhubi <kamal@marhubi.com>
2024-06-02 11:44:50 -0400

    fonts: Add future variant names for iosevka

--- modules/home-manager/fonts.nix
+++ modules/home-manager/fonts.nix
@@ -1,6 +1,9 @@
 {pkgs, ...}: {
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
+    # TODO: When upgrading to 24.05, switch.
+    # (iosevka-bin.override {variant = "SGr-IosevkaFixed";})
+    # (iosevka-bin.override {variant = "SGr-IosevkaFixedSlab";})
     (iosevka-bin.override {variant = "sgr-iosevka-fixed";})
     (iosevka-bin.override {variant = "sgr-iosevka-fixed-slab";})
     (nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})
