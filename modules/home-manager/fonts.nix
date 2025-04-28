464b8ce37d1ef427df3bce058e9682ba11ba607e
Kamal Al Marhubi <kamal@marhubi.com>
2025-04-28 10:41:52 -0400

    fonts: Add prot's aporetic build of Iosevka

--- modules/home-manager/fonts.nix
+++ modules/home-manager/fonts.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
+    unstable.aporetic
     (iosevka-bin.override {variant = "SGr-IosevkaFixed";})
     (iosevka-bin.override {variant = "SGr-IosevkaFixedSlab";})
     (nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})
