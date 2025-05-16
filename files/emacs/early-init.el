20e9e66626b5225b443ccb14a9d486558de88302
Kamal Al Marhubi <kamal@marhubi.com>
2025-05-15 23:23:22 -0400

    emacs: minor visual declutter

--- files/emacs/early-init.el
+++ files/emacs/early-init.el
@@ -13,3 +13,14 @@
   (startup-redirect-eln-cache
    (convert-standard-filename
     (expand-file-name "var/eln-cache/" k/emacs-data-directory))))
+
+;; Disable UI elements early to avoid momentary display
+(push '(vertical-scroll-bars . nil) default-frame-alist)
+(push '(horizontal-scroll-bars . nil) default-frame-alist)
+(setq scroll-bar-mode nil
+      horizontal-scroll-bar-mode nil)
+
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(setq tool-bar-mode nil
+      menu-bar-mode nil)
