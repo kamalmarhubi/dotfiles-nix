804975a038d720a27100b2d7c466c2aac1503b83
Kamal Al Marhubi <kamal@marhubi.com>
2025-05-25 16:23:02 -0400

    nix-darwin: clean up some cruft that is no longer necessary
    Setting shell seems to be fixed, and activation now needs to be run as
    root so the sudo timeout shenanigans are unnecessary. Unsure about the
    set-path removal; needs to be checked with a restart.

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -6,21 +6,23 @@
   ...
 }: {
   imports = [
-    ./sudo.nix
     ../unfree.nix
   ];
   system.stateVersion = 5;
   # services.karabiner-elements.enable = true;
-  # nix.configureBuildUsers = true;
+
+  # Set up touch id authentication for sudo.
+  security.pam.services.sudo_local.touchIdAuth = true;
+  # Require authentication on every sudo invocation.
+  environment.etc."sudoers.d/10-timestamp_timeout".text = ''
+    Defaults        timestamp_timeout=0
+  '';
   nix.settings.experimental-features = "nix-command flakes";
   environment.shells = [pkgs.fish];
   programs.fish.enable = true;
   users.users.kamal.home = "/Users/kamal";
   users.users.kamal.name = "kamal";
   users.users.kamal.shell = pkgs.fish;
-  system.activationScripts.postActivation.text = ''
-    dscl . -create '/Users/kamal' UserShell '/run/current-system/sw${pkgs.fish.shellPath}'
-  '';
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.users.kamal = import ../home-manager {extraModules = extraHomeModules;};
