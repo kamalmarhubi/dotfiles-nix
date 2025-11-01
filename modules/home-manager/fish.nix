b282472cbab0d66850f7a6995a255e29a4ff471a
Kamal Al Marhubi <kamal@marhubi.com>
2025-11-01 16:34:57 -0400

    fish: drop manually sourcing nix-daemon.fish

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -10,15 +10,5 @@
     enableTransience = true;
   };
 
-  # For whatever reason, the installer didn't put this somewhere that
-  # fish would see. Since the nix-daemon.fish file guards against
-  # double-sourcing, there's no harm including this in all systems.
-  xdg.configFile."fish/conf.d/nix.fish".text = ''
-    # Nix
-    if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
-      . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
-    end
-    # End Nix
-  '';
   xdg.configFile."starship.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/home-manager/files/starship.toml";
 }
