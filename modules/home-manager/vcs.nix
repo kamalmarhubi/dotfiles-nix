f7b6bc27b327ebcb052b898407a2a2f301faa32c
Kamal Al Marhubi <kamal@marhubi.com>
2025-11-01 16:32:22 -0400

    vcs: Use latest jj, drop sapliing

--- modules/home-manager/vcs.nix
+++ modules/home-manager/vcs.nix
@@ -4,12 +4,7 @@
   inputs,
   system,
   ...
-}: let
-  configDir =
-    if pkgs.stdenv.hostPlatform.isDarwin
-    then "Library/Application Support"
-    else config.xdg.configHome;
-in {
+}: {
   home = {
     shellAliases = {
       git = "git-branchless wrap --";
@@ -21,14 +16,14 @@ in {
       git-absorb
       git-lfs
       git-branchless
-      jujutsu # from unstable because version in stable as some CVE
+      # TODO(25.11): Consider going back to stable?
+      unstable.jujutsu # 0.34 is latest; stable nixpkgs-25.05-darwin is on 0.29
       # git-filter-repo
       lazygit
-      sapling # from unstable to avoid needing to build nodejs 20
     ];
   };
 
-  home.file."${configDir}/jj/config.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/home-manager/files/jjconfig.toml";
+  xdg.configFile."jj/config.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/home-manager/files/jjconfig.toml";
   xdg.configFile."git/config".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/home-manager/files/git/config";
   xdg.configFile."git/config.mine".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/home-manager/files/git/config.mine";
   xdg.configFile."git/config.system".text = let
