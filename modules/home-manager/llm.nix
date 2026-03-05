7b281b5befca8421b40851e89b763051d3ff829e
Kamal Al Marhubi <kamal@marhubi.com>
2026-03-05 16:00:23 -0500

    llm: add build deps for gondolin

--- modules/home-manager/llm.nix
+++ modules/home-manager/llm.nix
@@ -2,11 +2,18 @@
   pkgs,
   ...
 }: {
-  home.packages = with pkgs.llm-agents; [
-    chainlink
-    claude-code
-    claude-code-acp
-  ];
+  home.packages =
+    (with pkgs.llm-agents; [
+      chainlink
+      claude-code
+      claude-code-acp
+      # mcporter
+    ])
+    ++ (with pkgs; [
+      # zig and e2fsprogs for https://github.com/earendil-works/gondolin
+      e2fsprogs
+      zig
+    ]);
 
   nixpkgs.allowUnfreePackages = with pkgs.llm-agents; [
     claude-code
